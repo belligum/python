ecf57fcbee127642463561ba3d29a0d23f1e15cc
ronaldomessi <nivedhitasankaramoorthy@gmail.com>
2024-12-21 10:13:09 +0530

    python

---
+++ datatype.py
@@ -0,0 +1,25 @@
+name="sankeerthan"
+age=11
+weight=42.3
+student=True
+
+print("the datatype of name is",type(name))
+print("the datatype of age is",type(age))
+print("the datatype of weight is",type(weight))
+print("the datatype of student is",type(student))
+
+number1=int(input("enter the num1"))
+number2=int(input("enter the num2"))
+print("addition",number1+number2)
+print("subtraction",number1-number2)
+print("multiplication",number1*number2)
+print("divison",number1/number2)
+print("flow division",number1//number2)
+print("modules",number1%number2)
+print("square",number1**number2)
+
+
+a="football"
+b="boots"
+c=a+b
+print(c)
