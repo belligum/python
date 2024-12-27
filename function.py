577188c434c01ae42c6517fb7b025bc2f3cf6095
ronaldomessi <nivedhitasankaramoorthy@gmail.com>
2024-12-27 12:40:37 +0530

    function

---
+++ function.py
@@ -0,0 +1,28 @@
+a=input("enter your name")
+def greet():
+    print("hellow",a +"good morning")
+
+greet()
+
+def absolote(num):
+ if num>=0:
+    return num
+ else :
+    return -num
+print("absolote number of -105 is",absolote(-105))
+
+def add(num1,num2):
+  return num1+num2
+print("addition of both numbers is",add(23,654))
+
+def minus(num1,num2):
+   return num1-num2
+print("subtraction of both numbers is",minus(26,8765))
+
+def multiply(num1,num2):
+   return num1*num2
+print("multiplication of both numbers is",multiply(987654,908765))
+
+def divide(num1,num2):
+   return num1/num2
+print("divition of both the numbers is",divide(874512479461347946/85758552))
